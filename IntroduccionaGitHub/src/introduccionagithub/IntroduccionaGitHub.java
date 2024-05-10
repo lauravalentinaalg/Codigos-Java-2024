9bb153b71158db2a9695dcfb299ef9a743635ef6
Laura Valentina López García <86180451+lauravalentinaalg@users.noreply.github.com>
2024-05-10 13:01:48 -0500

    Update IntroduccionaGitHub.java
    esto no se hace pero bueno

--- IntroduccionaGitHub/src/introduccionagithub/IntroduccionaGitHub.java
+++ IntroduccionaGitHub/src/introduccionagithub/IntroduccionaGitHub.java
@@ -5,7 +5,7 @@ public class IntroduccionaGitHub {
 
     public static void main(String[] args) {
         System.out.println("Hola GitHub");
-        System.out.println("otra cosa");
+        System.out.println("otra cosaaaaa");
         System.out.println("algo más");
         int b=5;
     }
